d6a82af0b16b791293fb94dcff3bf950d9a07f2d
Ammar <59948271+aryannuha@users.noreply.github.com>
2025-04-20 22:35:32 +0700

    Add files via upload

---
+++ hmi/README.md
@@ -0,0 +1,7 @@
+# TA2025
+repository software TA2025
+
+- Program Dashboard HMI
+  Im using python to build dashboard hmi, Dash framework for data visualization, and spreadsheet as database
+- Program Arduino Instrumentation
+  Parameter that will this system read is temp, humidity, windspeed, and rainfall for microclimate station
---
+++ hmi/pages/th_out.py
@@ -0,0 +1,131 @@
+from dash import dcc, html, dash_table
+import dash_bootstrap_components as dbc
+
+th_out_layout = html.Div([
+    # NAVBAR
+    html.Div([
+        html.Div("T&H OUTDOOR DASHBOARD", className="navbar-title"),
+        html.Img(src="/static/icon/notification.svg", className="notification-icon"),
+    ], className="d-flex justify-content-between align-items-center p-3 border-bottom navbar-full"),
+
+    # TAMPILAN PARAMETER SENSOR GRID
+    html.Div([
+        dbc.Row([
+            dbc.Col(html.Div([
+                html.Div([
+                    html.Div([
+                        html.Img(src="/static/icon/temperature.svg", className="param-icon me-2"),
+                        html.Span("TEMPERATURE IN", className="param-title me-2"),
+                        html.Span(id="suhu-display-indoor", className="param-value")
+                    ], className="d-flex align-items-center mb-2"),
+                    html.Div([
+                        html.Img(src="/static/icon/humidity.svg", className="param-icon me-2"),
+                        html.Span("HUMIDITY IN", className="param-title me-2"),
+                        html.Span(id="kelembaban-display-indoor", className="param-value")
+                    ], className="d-flex align-items-center")
+                ])
+            ], className="param-card"), width=4),
+
+            dbc.Col(html.Div([
+                html.Div([
+                    html.Img(src="/static/icon/sun.svg", className="param-icon me-2"),
+                    html.Span("PAR", className="param-title me-2"),
+                    html.Span("15 µmol·m⁻²·s⁻¹", className="param-value")
+                ], className="d-flex align-items-center")
+            ], className="param-card"), width=4),
+
+            dbc.Col(html.Div([
+                html.Div([
+                    html.Img(src="/static/icon/co.svg", className="param-icon me-2"),
+                    html.Span("CO2", className="param-title me-2"),
+                    html.Span(id="co2-display", className="param-value")
+                ], className="d-flex align-items-center")
+            ], className="param-card"), width=4),
+        ], className="mb-3"),
+
+        dbc.Row([
+            dbc.Col(html.Div([
+                html.Div([
+                    html.Div([
+                        html.Img(src="/static/icon/temperature.svg", className="param-icon me-2"),
+                        html.Span("TEMPERATURE OUT", className="param-title me-2"),
+                        html.Span(id="suhu-display-outdoor", className="param-value")
+                    ], className="d-flex align-items-center mb-2"),
+                    html.Div([
+                        html.Img(src="/static/icon/humidity.svg", className="param-icon me-2"),
+                        html.Span("HUMIDITY OUT", className="param-title me-2"),
+                        html.Span(id="kelembaban-display-outdoor", className="param-value")
+                    ], className="d-flex align-items-center")
+                ])
+            ], className="param-card"), width=4),
+
+            dbc.Col(html.Div([
+                html.Div([
+                    html.Img(src="/static/icon/windspeed.svg", className="param-icon me-2"),
+                    html.Span("WINDSPEED", className="param-title me-2"),
+                    html.Span(id="windspeed-display", className="param-value")
+                ], className="d-flex align-items-center")
+            ], className="param-card"), width=4),
+
+            dbc.Col(html.Div([
+                html.Div([
+                    html.Img(src="/static/icon/rainfall.svg", className="param-icon me-2"),
+                    html.Span("RAINFALL", className="param-title me-2"),
+                    html.Span(id="rainfall-display", className="param-value")
+                ], className="d-flex align-items-center")
+            ], className="param-card"), width=4),
+        ])
+    ], className="container my-3"),
+
+    # TAMPILAN BOTTOM GRID
+    html.Div([
+        dbc.Row([
+            dbc.Col(html.Img(src="/static/img/gh.jpg", className="greenhouse-img"), width=6),
+            dbc.Col([
+
+                # # Alarm Section
+                # html.Div([
+                #     dbc.Row([
+                #         dbc.Col([html.Div(className="alarm-dot red"), html.Div("T&H", className="text-center")]),
+                #         dbc.Col([html.Div(className="alarm-dot yellow"), html.Div("PAR", className="text-center")]),
+                #         dbc.Col([html.Div(className="alarm-dot green"), html.Div("CO2", className="text-center")]),
+                #         dbc.Col([html.Div(className="alarm-dot red"), html.Div("VOLT", className="text-center")]),
+                #         dbc.Col([html.Div(className="alarm-dot green"), html.Div("CURRENT", className="text-center")]),
+                #         dbc.Col([html.Div(className="alarm-dot yellow"), html.Div("POWER", className="text-center")])
+                #     ], className="text-center mb-3")
+                # ]),
+
+                # Table Section
+                html.Div([
+                    html.H4("Real Time Data", className="text-center mb-2"),
+                    dash_table.DataTable(
+                        columns=[
+                            {"name": i, "id": i} for i in [
+                                "Temp (°C)", "Humidity (%)", "PAR (2-s-1)", "CO2 (PPM)", "Volt (V)", "Current (A)", "Power (Watt)"
+                            ]
+                        ],
+                        data=[],
+                        style_table={'overflowX': 'auto'},
+                        style_cell={"textAlign": "center"}
+                    )
+                ], className="data-table mb-3"),
+
+                # Button Section
+                html.Div([
+                    html.Button("SETTING", className="btn btn-secondary m-1"),
+                    dcc.Link("MAIN MENU", href="/dash/", className="btn btn-secondary m-1"),
+                    dcc.Link("T&H INDOOR", href="/dash/th-in", className="btn btn-secondary m-1"),
+                    dcc.Link("PAR", href="/dash/par", className="btn btn-secondary m-1"),
+                    dcc.Link("CO2", href="/dash/co2", className="btn btn-secondary m-1"),
+                    dcc.Link("T&H OUTDOOR", href="/dash/th-out", className="btn btn-secondary m-1"),
+                    dcc.Link("WINDSPEED", href="/dash/windspeed", className="btn btn-secondary m-1"),
+                    dcc.Link("RAINFALL", href="/dash/rainfall", className="btn btn-secondary m-1"),
+                    html.Button("LOGOUT", id="logout-button", className="btn btn-dark m-1"),
+                    dcc.Location(id="logout-redirect", refresh=True)  # Handles redirection
+                ], className="d-flex flex-wrap justify-content-end")
+            ], width=6)
+        ], className="g-2")
+    ], className="container mb-5"),
+
+    dcc.Interval(id='interval', interval=1100, n_intervals=0)
+])
