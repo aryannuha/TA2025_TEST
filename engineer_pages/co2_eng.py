fda5f7388d6f420fe4f58f9376d707f69556c991
Ammar <59948271+aryannuha@users.noreply.github.com>
2025-05-09 09:30:46 +0700

    Update co2_eng.py

--- engineer_pages/co2_eng.py
+++ engineer_pages/co2_eng.py
@@ -5,8 +5,8 @@ engineer_co2_layout = html.Div([
     # NAVBAR
     html.Div([
         html.Div("CO2 DASHBOARD", className="navbar-title"),
-        dcc.Link(html.Img(src="/static/icon/gps.svg", className="gps-icon me-2"), href="/dash/gps"),
-        dcc.Link(html.Img(src="/static/icon/notification.svg", className="notification-icon"), href="/dash/alarm"),
+        dcc.Link(html.Img(src="/static/icon/gps.svg", className="gps-icon me-2"), href="/dash/engineer/gps"),
+        dcc.Link(html.Img(src="/static/icon/notification.svg", className="notification-icon"), href="/dash/engineer/alarm"),
     ], className="d-flex justify-content-between align-items-center p-3 border-bottom navbar-full mb-1"),
     
     # MAIN CONTENT
