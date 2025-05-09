9793e3832cdf8aac2c28165e83dd6ca9a6c7fc57
Ammar <59948271+aryannuha@users.noreply.github.com>
2025-05-09 09:32:24 +0700

    Update th_out_eng.py

--- engineer_pages/th_out_eng.py
+++ engineer_pages/th_out_eng.py
@@ -5,8 +5,8 @@ engineer_th_out_layout = html.Div([
     # NAVBAR
     html.Div([
         html.Div("T&H OUTDOOR DASHBOARD", className="navbar-title"),
-        dcc.Link(html.Img(src="/static/icon/gps.svg", className="gps-icon me-2"), href="/dash/gps"),
-        dcc.Link(html.Img(src="/static/icon/notification.svg", className="notification-icon"), href="/dash/alarm"),
+        dcc.Link(html.Img(src="/static/icon/gps.svg", className="gps-icon me-2"), href="/dash/engineer/gps"),
+        dcc.Link(html.Img(src="/static/icon/notification.svg", className="notification-icon"), href="/dash/engineer/alarm"),
     ], className="d-flex justify-content-between align-items-center p-3 border-bottom navbar-full mb-1"),
     
     # MAIN CONTENT
