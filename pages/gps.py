151e83959ac75613bd37303110d33e5d79403c22
Ammar <59948271+aryannuha@users.noreply.github.com>
2025-05-16 20:49:39 +0700

    Update gps.py

--- pages/gps.py
+++ pages/gps.py
@@ -66,6 +66,7 @@ gps_layout = html.Div([
         # Button Section
         html.Div([
             html.Button("SETTING", className="btn btn-secondary m-1"),
+            dcc.Link("MCS", href="/dash/", className="btn btn-secondary m-1"),
             dcc.Link("T&H INDOOR", href="/dash/th-in", className="btn btn-secondary m-1"),
             dcc.Link("PAR", href="/dash/par", className="btn btn-secondary m-1"),
             dcc.Link("CO2", href="/dash/co2", className="btn btn-secondary m-1"),
